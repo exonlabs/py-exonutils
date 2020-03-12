5ab5957738274d3f45c27bdd0e5647f3147e786c
M.Mohsen <eng.mmsayed@gmail.com>
2020-03-12 18:14:42 +0200

    added python2 support

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@
     :license: BSD, see LICENSE for more details.
 """
 import os
+import sys
 from setuptools import setup
 
 os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 __PKGNAME__ = 'exonutils'
 __VERSION__ = '1.0.1.dev'
+
+PY3 = sys.version_info >= (3, 0)
 
 
 setup(
@@ -23,15 +26,15 @@ setup(
     include_package_data=True,
     zip_safe=False,
     platforms='linux',
-    python_requires='>=3.5',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     install_requires=[
         'setproctitle>=1.1.10',
         'simplejson>=3.17.0',
-        'sqlalchemy>=1.3.13',
+        'sqlalchemy>=1.3.15',
         'alembic>=1.4.0',
         'flask>=1.1.1',
         'Jinja2>=2.11.1',
-        'gunicorn>=20.0.4',
+        'gunicorn>=%s' % ('20.0.4' if PY3 else '19.10.0'),
         'gevent>=1.4.0',
     ],
     classifiers=[
@@ -41,7 +44,8 @@ setup(
         'Operating System :: POSIX',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
