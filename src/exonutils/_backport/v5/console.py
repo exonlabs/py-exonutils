a8e6e0d1894bd30365be767c914e92a14c284a3e
M.Mohsen <mmsayed.dev@gmail.com>
2022-10-25 16:38:29 +0200

    added v5 backport libs

---
+++ src/exonutils/_backport/v5/console.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+import re
+from getpass import getpass
+try:
+    import readline
+    readline.set_completer_delims(' \t\n;')
+    readline.parse_and_bind("tab: complete")
+except:
+    pass
+try:
+    import colorama
+    colorama.init()
+except ImportError:
+    colorama = None
+
+__all__ = []
+
+
+class ConsoleInput(object):
+
+    prompt_caret = '>>'
+
+    @classmethod
+    def _input(cls, msg, default='', hidden=False):
+        if colorama:
+            # print BOLD/BRIGHT text message
+            prompt = "%s%s %s %s" % (
+                colorama.Style.BRIGHT,
+                cls.prompt_caret,
+                msg,
+                colorama.Style.RESET_ALL)
+        else:
+            prompt = "%s %s " % (cls.prompt_caret, msg)
+
+        result = (getpass(prompt) if hidden else input(prompt)).strip()
+        return result if result else default
+
+    @classmethod
+    def _err(cls, msg):
+        if colorama:
+            # print RED color error message
+            print("%s -- %s%s" % (
+                colorama.Fore.RED + colorama.Style.BRIGHT,
+                msg,
+                colorama.Style.RESET_ALL))
+        else:
+            print(" -- %s" % msg)
+
+    @classmethod
+    def _validator(cls, input_str, regex=None):
+        if regex and not re.search(regex, input_str):
+            return "invalid input format"
+        return True
+
+    @classmethod
+    def get(cls, msg, default=None, required=False, trials=3, hidden=False,
+            regex=None, validator=None):
+        str_default = '' if default is None else str(default).strip()
+
+        msg = "%s:" % msg
+        if not required or str_default:
+            msg += " [%s]" % str_default
+
+        if not validator:
+            validator = cls._validator
+
+        for i in range(max(1, trials)):
+            input_str = cls._input(msg, default=str_default, hidden=hidden)
+            if input_str:
+                chk = validator(input_str, regex=regex)
+                if chk is True:
+                    return input_str
+                cls._err(chk)
+            elif not required:
+                return None if default is None else str_default
+            else:
+                cls._err("required input, please enter value")
+
+        raise ValueError("failed to get valid input")
+
+    @classmethod
+    def passwd(cls, *args, **kwargs):
+        kwargs.update({'hidden': True})
+        return cls.get(*args, **kwargs)
+
+    @classmethod
+    def confirm_passwd(cls, msg, value, trials=3):
+        for i in range(max(1, trials)):
+            res = cls._input("%s:" % msg, hidden=True)
+            if res:
+                if res == str(value):
+                    return True
+                cls._err("value not matching, please try again")
+            else:
+                cls._err("empty input, please confirm value")
+
+        raise ValueError("failed to confirm value")
+
+    @classmethod
+    def number(cls, msg, default=None, required=False, trials=3,
+               vmin=None, vmax=None):
+        def validator(input_str, regex=None):
+            if not re.search('^[0-9-]+$', input_str):
+                return "invalid number format"
+            num = int(input_str)
+            if (vmin is not None and (int(vmin) - num) > 0) or \
+               (vmax is not None and (int(vmax) - num) < 0):
+                return "value out of range"
+            return True
+
+        res = cls.get(
+            msg, default=default, required=required, trials=trials,
+            validator=validator)
+        return int(res) if res is not None else None
+
+    @classmethod
+    def decimal(cls, msg, default=None, required=False, trials=3,
+                vmin=None, vmax=None):
+        def validator(input_str, regex=None):
+            if not re.search('^[0-9-]+(.[0-9]+)?$', input_str):
+                return "invalid decimal format"
+            num = float(input_str)
+            if (vmin is not None and (float(vmin) - num) > 0) or \
+               (vmax is not None and (float(vmax) - num) < 0):
+                return "value out of range"
+            return True
+
+        res = cls.get(
+            msg, default=default, required=required, trials=trials,
+            validator=validator)
+        return float(res) if res is not None else None
+
+    @classmethod
+    def select(cls, msg, values, default=None, required=False, trials=3,
+               case_sensitive=False):
+        if case_sensitive:
+            values = [str(s) for s in values]
+            if default is not None:
+                default = str(default)
+        else:
+            values = [str(s).lower() for s in values]
+            if default is not None:
+                default = str(default).lower()
+
+        msg = "%s {%s}" % (msg, '|'.join(values))
+        if default not in values:
+            default = None
+
+        def validator(input_str, regex=None):
+            if not case_sensitive:
+                input_str = input_str.lower()
+            if input_str not in values:
+                return "invalid value, please select from list"
+            return True
+
+        return cls.get(
+            msg, default=default, required=required, trials=trials,
+            validator=validator)
+
+    @classmethod
+    def yesno(cls, msg, default=None, required=False, trials=3):
+        res = cls.select(
+            msg, ['y', 'n'], default=default, required=required,
+            trials=trials, case_sensitive=False)
+        return bool(res == 'y')
---
+++ src/exonutils/_backport/v5/webapp.py
@@ -0,0 +1,185 @@
+# -*- coding: utf-8 -*-
+import os
+import uuid
+import signal
+import logging
+from flask import Flask, request, jsonify
+from jinja2 import BaseLoader
+from traceback import format_exc
+
+try:
+    import colorama
+    colorama.init()
+except ImportError:
+    pass
+
+__all__ = []
+
+
+class BaseWebSrv(object):
+
+    def __init__(self, name=None, options={}, logger=None,
+                 req_logger=None, debug=0):
+        self.name = name if name else self.__class__.__name__.lower()
+        self.proctitle = self.name
+        self.options = options
+        self.debug = debug
+        self.base_path = ''
+        self.tpl_loader = None
+        self.app = None
+
+        # websrv logger
+        self.log = logger if logger else logging.getLogger()
+        # websrv requests logger
+        self.reqlog = req_logger if req_logger else \
+            logging.getLogger('%s.requests' % self.log.name)
+        self.reqlog.setLevel(logging.INFO)
+        self.reqlog.propagate = False
+        if not self.reqlog.handlers:
+            self.reqlog.handlers = [logging.NullHandler()]
+
+    def initialize(self):
+        self.log.info("Initializing")
+        self.app = self.create_app()
+
+    def load_views(self, views):
+        # initialize view
+        for V in views:
+            if not issubclass(V, BaseWebView):
+                raise RuntimeError("invalid view: %s" % str(V))
+
+            v = V(self)
+            if hasattr(v, 'initialize'):
+                v.initialize()
+            for url, endpoint in v.routes:
+                self.app.add_url_rule(
+                    url, endpoint=endpoint,
+                    view_func=v.dispatch_request,
+                    methods=v.methods)
+
+    def create_app(self):
+        # check and adjust app options
+        if not self.options.get('secret_key', None):
+            self.options['secret_key'] = uuid.uuid5(
+                uuid.uuid1(), uuid.uuid4().hex).hex
+        if not self.options.get('max_content_length', None):
+            self.options['max_content_length'] = 10485760  # 10 MiB
+        self.options['trap_http_exceptions'] = True
+        self.options['trap_bad_request_errors'] = True
+
+        # create flask app
+        app = Flask(
+            self.name,
+            template_folder=os.path.join(self.base_path, 'templates'),
+            static_folder=os.path.join(self.base_path, 'static'))
+
+        # update app config from options
+        for k, v in self.options.items():
+            app.config[k.upper()] = v
+
+        # set jinja options
+        app.jinja_env.autoescape = True
+        app.jinja_env.auto_reload = app.config.get('TEMPLATES_AUTO_RELOAD')
+        if self.tpl_loader and isinstance(self.tpl_loader, BaseLoader):
+            app.jinja_loader = self.tpl_loader
+
+        # register exception handler
+        @app.errorhandler(Exception)
+        def exception_handler(e):
+            if hasattr(e, 'name') and hasattr(e, 'code'):
+                return self.handle_response((e.name, e.code))
+            else:
+                self.log.error(format_exc().strip())
+                return self.handle_response(("Internal Server Error", 500))
+
+        return app
+
+    # response handler
+    def handle_response(self, result):
+        return result
+
+    def start(self, host, port):
+        # adjust request logs
+        logging.getLogger('werkzeug').parent = self.reqlog
+
+        # process PID
+        self.root_pid = os.getpid()
+
+        self.app.run(
+            host=host, port=port,
+            debug=bool(self.debug >= 1),
+            use_reloader=bool(self.debug >= 3))
+
+    def stop(self):
+        os.kill(self.root_pid, signal.SIGTERM)
+
+
+class BaseRESTWebSrv(BaseWebSrv):
+
+    def handle_response(self, response):
+        if type(response) is tuple:
+            data, code = response[0], response[1]
+        else:
+            data, code = response, 200
+
+        if type(data) is not dict:
+            key = 'error' if code >= 400 else 'result'
+            data = {key: data}
+
+        return self.format_response(data, code)
+
+    # default response parser is to send JSON data results
+    def format_response(self, data, status):
+        return (jsonify(**data), status)
+
+
+class BaseWebView(object):
+    routes = []
+    methods = ['GET', 'POST']
+
+    def __init__(self, websrv):
+        self.name = self.__class__.__name__
+        self.websrv = websrv
+        self.debug = websrv.debug
+
+        # view logger
+        self.log = logging.getLogger(self.name)
+        self.log.parent = websrv.log
+
+    # def initialize(self):
+    #     pass
+
+    def dispatch_request(self, *args, **kwargs):
+        # exec before request handlers
+        if hasattr(self, 'before_request'):
+            result = self.before_request()
+            if result is not None:
+                return self.websrv.handle_response(result)
+
+        result = self.run_request(
+            request.method.lower(), *args, **kwargs)
+
+        # exec after request handlers
+        if hasattr(self, 'after_request'):
+            result = self.after_request(result)
+
+        return self.websrv.handle_response(result)
+
+    def run_request(self, method, *args, **kwargs):
+        if not hasattr(self, method):
+            # use GET method instead of HEAD if no handler
+            if method == 'head' and hasattr(self, 'get'):
+                method = 'get'
+            else:
+                return self.websrv.handle_response(
+                    ("Method Not Allowed", 405))
+
+        return getattr(self, method)(*args, **kwargs)
+
+    # @classmethod
+    # def before_request(cls):
+    #     return None
+
+    # @classmethod
+    # def after_request(cls, response):
+    #     return response
